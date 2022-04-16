ce50745f80059731c35aed66ffbf24d89f6900df
coyote <megan.metelak@gmail.com>
2022-04-16 07:35:46 -0500

    projects and portfolio index

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React from "react";
+
+return (
+  <div>
+    <div className="flex-row">
+      {projects.map((project, idx) => {
+        <project project={project} key={"project" + idx} />;
+      })}
+    </div>
+  </div>
+);
+
+export default Portfolio;
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,1 +1,25 @@
-import React from 'react';
+import React from "react";
+import { removeHyphenAndCapitalize } from "../../utils/helpers";
+
+function Project({ project }) {
+  const { name, repo, link, description } = project;
+
+  return (
+    <div className="project" key={name}>
+      <img
+        src={require("../../assets/projects/${name}.jpg")}
+        className="project-bg"
+      />
+      <div className="project-text">
+          <h3>
+              <a href={repo}>
+                  <i className="fab fa-github"></i>
+              </a>
+          </h3>
+          <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
